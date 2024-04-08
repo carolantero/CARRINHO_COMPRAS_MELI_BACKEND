7706261194f17cb9b6f0b88454191158651692fd
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-08 17:56:35 -0300

    feat: criando endpoint do tipo GET para trazer todos os produtos

--- src/controller.py
+++ src/controller.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
+from src.repository import RepositoryShoppingCart
 
 router = APIRouter()
 
 @router.get("/get_all_products", tags=["Produtos"])
 def controller_get_all_products():
     try:
-        result = True
+        repository: RepositoryShoppingCart = RepositoryShoppingCart()
+        result: list = repository.get_all_products()
 
         return result
     except Exception as e:
