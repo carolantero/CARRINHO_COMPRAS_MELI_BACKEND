f36da92bd5221e1db13b9a27cbac19abb9c2bf20
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-08 17:58:53 -0300

    feat: criando metodos para buscar e tratar dados dos produtos de api externa

---
+++ src/repository.py
@@ -0,0 +1,48 @@
+import requests
+from fastapi import HTTPException
+
+class RepositoryShoppingCart:
+    # def __init__() -> None:
+    #     ...
+
+    def _get_data_from_meliapi(self) -> list:
+        try:
+            category: str = 'eletrodomestico'
+            url: str = f'https://api.mercadolibre.com/sites/MLB/search?q={category}'
+            response: requests.models.Response = requests.get(url)
+            data: dict = response.json()
+
+            result: list = data['results'][:30]
+             
+            return result
+        except Exception as e:
+                raise e
+        
+    def _organize_products_list(self, products_list: list) -> list:
+        try:
+            result: list = []
+
+            for item in products_list:
+                format_dict: dict = {
+                    "product_id": item["id"],
+                    "product_title": item["title"],
+                    "product_image": item["thumbnail"],
+                    "product_price": item["price"]
+                }
+                result.append(format_dict)
+            
+            return result
+        except Exception as e:
+                raise e
+         
+
+    def get_all_products(self) -> list:
+        try:
+            data_products: list = self._get_data_from_meliapi()
+
+            result: list = self._organize_products_list(products_list=data_products) 
+
+             
+            return result
+        except Exception as e:
+                raise e
