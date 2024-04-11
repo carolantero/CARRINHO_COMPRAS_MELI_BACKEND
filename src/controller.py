629dd47470da45fc0deb9590cac4c08b4f0b30e3
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-11 18:06:00 -0300

    docs: adicionando docstring no arquivo de controller para melhor entendimento da finalidade de cada rota

--- src/controller.py
+++ src/controller.py
@@ -9,6 +9,11 @@ repository = RepositoryShoppingCart()
 
 @router.get("/get_all_products", tags=["Produtos"])
 def controller_get_all_products():
+    """
+    Obtém todos os produtos disponíveis.
+
+    Retorna: Uma lista de dicionários com todos os produtos.
+    """
     try:
         result: list = repository.get_all_products()
 
@@ -18,6 +23,12 @@ def controller_get_all_products():
     
 @router.post("/post_cart_products/", tags=["Adicionar Itens ao Carrinho"])
 def controller_post_cart_products(items: List[ShoppingCartItem]):
+    """
+    Adiciona itens ao carrinho de compras.
+
+    Args: items: Uma lista de objetos ShoppingCartItem representando os itens a serem adicionados ao carrinho.
+    Retorna: Uma lista de dicionários representando os itens adicionados ao carrinho.
+    """
     try:
         result: list = repository.post_cart_products(items)
 
@@ -27,6 +38,11 @@ def controller_post_cart_products(items: List[ShoppingCartItem]):
     
 @router.get("/get_cart_products/", tags=["Obter Itens do Carrinho"])
 def controller_get_cart_products():
+    """
+    Obtém os itens adicionados no carrinho de compras.
+
+    Retorna: Uma lista de dicionários representando os itens dentro do carrinho de compras.
+    """
     try:
         result: list = repository.get_cart_products()
 
