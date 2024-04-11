f812f60e431484b2e2e106f2b8dc32a104aa85e1
Carolina Antero <carolina.antero.santos@gmail.com>
2024-04-11 18:06:50 -0300

    docs: adicionando docstring no arquivo repository para melhor entendimento da finalidade de cada método

--- src/repository.py
+++ src/repository.py
@@ -3,9 +3,17 @@ from fastapi import HTTPException
 
 class RepositoryShoppingCart:
     def __init__(self):
+        """
+        Inicializa um novo objeto RepositoryShoppingCart.
+        """
         self.shopping_cart = []
 
     def _get_data_from_meliapi(self) -> list:
+        """
+        Obtém dados da API externa (acesse: 'https://developers.mercadolibre.com/' para saber mais), para produtos de uma determinada categoria.
+
+        Retorna: Uma lista de dicionários representando os produtos obtidos.
+        """
         try:
             category: str = 'eletrodomestico'
             url: str = f'https://api.mercadolibre.com/sites/MLB/search?q={category}'
@@ -19,6 +27,12 @@ class RepositoryShoppingCart:
                 raise e
         
     def _organize_products_list(self, products_list: list) -> list:
+        """
+        Organiza a lista de produtos recebida em um formato específico.
+
+        Args: products_list: Uma lista de dicionários representando os produtos que devem ser formatados.
+        Retorna: Uma lista de dicionários representando os produtos já formatados.
+        """
         try:
             result: list = []
 
@@ -37,6 +51,11 @@ class RepositoryShoppingCart:
          
 
     def get_all_products(self) -> list:
+        """
+        Obtém todos os produtos disponíveis da API externa e os organiza.
+
+        Retorna: Uma lista de dicionários representando os produtos organizados e formatados.
+        """
         try:
             data_products: list = self._get_data_from_meliapi()
 
@@ -48,6 +67,12 @@ class RepositoryShoppingCart:
                 raise e
         
     def post_cart_products(self, items: list) -> list:
+        """
+        Adiciona itens ao carrinho de compras.
+
+        Args: items: Uma lista de dicionários representando os itens a serem adicionados ao carrinho.
+        Retorna: Uma lista de dicionários representando os itens adicionados ao carrinho.
+        """
         try:
             self.shopping_cart.clear()
             self.shopping_cart.extend(items)
@@ -61,6 +86,11 @@ class RepositoryShoppingCart:
 
 
     def get_cart_products(self) -> list:
+        """
+        Obtém os itens do carrinho de compras.
+
+        Retorna: Uma lista de dicionários representando os itens no carrinho de compras.
+        """
         try:  
             result: list = self.shopping_cart
 
