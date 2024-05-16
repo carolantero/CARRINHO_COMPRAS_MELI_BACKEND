c601bbfe809a3ca8d69bd58decb645fb311f45bd
Carolina Antero <carolina.antero.santos@gmail.com>
2024-05-16 13:50:35 -0300

    chore: adicionando rota de buscar produtos por categoria e arrumando tratamento de Exception

--- src/controller.py
+++ src/controller.py
@@ -12,40 +12,75 @@ def controller_get_all_products():
     """
     Obtém todos os produtos disponíveis.
 
-    Retorna: Uma lista de dicionários com todos os produtos.
+    Retorna:
+        Uma lista de dicionários com todos os produtos.
+
+    Lança:
+        HTTPException: Se ocorrer algum erro ao tentar obter os produtos.
     """
     try:
         result: list = repository.get_all_products()
 
         return result
     except Exception as e:
-        raise e
+        raise HTTPException(status_code=500, detail="Erro interno do servidor ao tentar obter todos os produtos.") from e
+    
+@router.get("/get_products_by_category", tags=["Produtos por Categoria"])
+def controller_get_products_by_category(category: str):
+    """
+    Obtém os produtos disponíveis de acordo com sua categoria.
+
+    Args:
+        category: uma categoria do tipo 'str' que será usada como parametro na url da api externa.
+        
+    Retorna:
+        Uma lista de dicionários com todos os produtos.
+
+    Lança:
+        HTTPException: Se a categoria não for válida ou não retornar resultados.
+    """
+    try:
+        result: list = repository.get_products_by_category(category=category)
+
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Erro interno do servidor.") from e
     
 @router.post("/post_cart_products/", tags=["Adicionar Itens ao Carrinho"])
 def controller_post_cart_products(items: List[ShoppingCartItem]):
     """
     Adiciona itens ao carrinho de compras.
 
-    Args: items: Uma lista de objetos ShoppingCartItem representando os itens a serem adicionados ao carrinho.
-    Retorna: Uma lista de dicionários representando os itens adicionados ao carrinho.
+    Args:
+        items: Uma lista de objetos ShoppingCartItem representando os itens a serem adicionados ao carrinho.
+
+    Retorna:
+        Uma lista de dicionários representando os itens adicionados ao carrinho.
+
+    Lança:
+        HTTPException: Se ocorrer algum erro ao tentar adicionar os itens ao carrinho.
     """
     try:
         result: list = repository.post_cart_products(items=items)
 
         return result
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="Erro interno do servidor ao tentar adicionar itens ao carrinho.") from e
     
 @router.get("/get_cart_products/", tags=["Obter Itens do Carrinho"])
 def controller_get_cart_products():
     """
     Obtém os itens adicionados no carrinho de compras.
 
-    Retorna: Uma lista de dicionários representando os itens dentro do carrinho de compras.
+    Retorna:
+        Uma lista de dicionários representando os itens dentro do carrinho de compras.
+
+    Lança:
+        HTTPException: Se ocorrer algum erro ao tentar obter os itens do carrinho.
     """
     try:
         result: list = repository.get_cart_products()
 
         return result
     except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail="Erro interno do servidor ao tentar obter os itens do carrinho.") from e
